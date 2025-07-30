21f58338bc70fb8bf2c87eecb9413d228a376a44
sujeet <sujeet@infraspec.dev>
2025-07-31 00:23:43 +0530

    Feature: Count live neighbours

--- gameOfLife.go
+++ gameOfLife.go
@@ -16,3 +16,27 @@ func NewBoard(input [][]int, m, n int) *Board {
 	}
 	return &Board{matrix: mat}
 }
+
+var directions = [][]int{
+	{-1, -1}, {-1, 0}, {-1, 1},
+	{0, -1}, {0, 1},
+	{1, -1}, {1, 0}, {1, 1},
+}
+
+func countLiveNeighbors(board [][]int, row, col int) int {
+	m := len(board)
+	n := len(board[0])
+	liveCount := 0
+
+	for _, dir := range directions {
+		newRow := row + dir[0]
+		newCol := col + dir[1]
+
+		if newRow >= 0 && newRow < m && newCol >= 0 && newCol < n {
+			if board[newRow][newCol] == 1 || board[newRow][newCol] == -1 {
+				liveCount++
+			}
+		}
+	}
+	return liveCount
+}
--- gameOfLife_test.go
+++ gameOfLife_test.go
@@ -18,3 +18,33 @@ func TestNewBoard(t *testing.T) {
 		t.Errorf("Board should be created with %v*%v size", m, n)
 	}
 }
+
+func TestCountLiveNeighborsTopLeftCorner(t *testing.T) {
+	board := [][]int{
+		{0, 1, 0},
+		{1, 1, 0},
+		{0, 0, 1},
+	}
+
+	got := countLiveNeighbors(board, 0, 0)
+	want := 3
+
+	if got != want {
+		t.Errorf("countLiveNeighbors(0,0) = %d; want %d", got, want)
+	}
+}
+
+func TestCountLiveNeighborsTopMiddle(t *testing.T) {
+	board := [][]int{
+		{0, 1, 0},
+		{1, 1, 0},
+		{0, 0, 1},
+	}
+
+	got := countLiveNeighbors(board, 0, 1)
+	want := 2
+
+	if got != want {
+		t.Errorf("countLiveNeighbors(0,1) = %d; want %d", got, want)
+	}
+}
