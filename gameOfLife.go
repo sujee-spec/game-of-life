9b2e4216b909e1cee18c634796196142ad25aa81
sujeet <sujeet@infraspec.dev>
2025-07-30 23:48:40 +0530

    Creation of board

---
+++ gameOfLife.go
@@ -0,0 +1,16 @@
+package gameoflife
+
+type board struct{
+	matrix matrix
+}
+
+type matrix [][]int
+
+func NewBoard(m,n int) *board {
+	matrix:= make([][]int,m)
+
+	for i:= range matrix{
+		matrix[i]= make([]int, n)
+	}
+	return &board{matrix}
+}
