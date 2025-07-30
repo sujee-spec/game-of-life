def1fab52a0b59f112bd3e4b2e3c78ed5544eb45
sujeet <sujeet@infraspec.dev>
2025-07-31 01:06:22 +0530

    Removed file main and refactored code

--- gameOfLife.go
+++ gameOfLife.go
@@ -1,16 +1,16 @@
 package gameoflife
 
-type board struct{
+type board struct {
 	matrix matrix
 }
 
 type matrix [][]int
 
-func NewBoard(m,n int) *board {
-	matrix:= make([][]int,m)
+func NewBoard(m, n int) *board {
+	matrix := make([][]int, m)
 
-	for i:= range matrix{
-		matrix[i]= make([]int, n)
+	for i := range matrix {
+		matrix[i] = make([]int, n)
 	}
 	return &board{matrix}
 }
--- gameOfLife_test.go
+++ gameOfLife_test.go
@@ -1,15 +1,15 @@
 package gameoflife
 
-import(
+import (
 	"testing"
 )
 
-func TestNewBoard(t *testing.T){
-	m:=4
-	n:=3
-	b:= NewBoard(m,n)
-	if b==nil{
-		t.Errorf("Board should be created with %v*%v size",m,n)
+func TestNewBoard(t *testing.T) {
+	m := 4
+	n := 3
+	b := NewBoard(m, n)
+	if b == nil {
+		t.Errorf("Board should be created with %v*%v size", m, n)
 	}
-	
+
 }
